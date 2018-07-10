b4b93b17b13fb32577c76ba409f8e9bf4c77f97f
Tim Smith <tgsmith489@gmail.com>
2018-07-09 20:10:07 -0500

    More work on form

--- src/components/contactform/index.js
+++ src/components/contactform/index.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import './index.scss';
 
-export default class Contact extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+export default (props) => (
+  <div>
+    <iframe name="hide" className='is-hidden'></iframe>
+    <form name='contact' target='hide' method="POST" id='contact' data-netlify="true" data-netlify-honeypot="bot-field">
+      <input type="hidden" name="bot-field" />
 
-  render() {
-    return (
-      <div>
-        <iframe name="hide" className='is-hidden'></iframe>
-        <form name='contact' target='hide' method="POST" id='contact' data-netlify="true" data-netlify-honeypot="bot-field">
-          <input type="hidden" name="bot-field" />
+      <p id='formResponse'>Thanks for reaching out to me. I'll be in contact as soon as possible!</p>
+      
+      <input type="text" name="name" placeholder="Name" required />
+      <input type="text" name="email" placeholder="Email" required />
+      <p style={{display:'none'}}>Empty: <input type="text" name="url" /></p>
+      <textarea name="message" placeholder="Your Message" required></textarea>
+      <input type="submit" name="submit" value={props.buttonText} onClick={() => formSumbit()} />
+    </form>
+  </div>
+)
 
-          <p id='formResponse'>Thanks for reaching out to me. I'll be in contact as soon as possible!</p>
-          <input type="text" name="name" placeholder="Name" required />
-          <input type="text" name="email" placeholder="Email" required />
-          <p style={{display:'none'}}>Empty: <input type="text" name="url" /></p>
-          <textarea name="message" placeholder="Your Message" required></textarea>
-          <input type="submit" name="submit" value={this.props.buttonText} />
-      </form>
-      </div>
-    )
-  }
+const formSumbit = () => {
+  setTimeout( () => {
+    document.getElementById('contact').reset();
+    document.getElementById('formResponse').style.display = 'block';
+    document.getElementById('formResponse').style.opacity = '1';
+  }, 1000)
 }
