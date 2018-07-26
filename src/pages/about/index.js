29bd10eac3a2796dabe1d874a061f3d08bb0a4c8
Tim Smith <tgsmith489@gmail.com>
2018-07-26 15:38:51 -0500

    Update index.js

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -17,7 +17,7 @@ export default () => {
             <p>My name is Tim Smith and I'm a front end web developer. I build responsive websites using modern technologies like React, Vue, Sass, Git, as well as good old jQuery. I also have experience with PHP and Wordpress as a CMS platform.<br/><br/>
             When I'm not building things for the web, I enjoy spending time with my wife and daughter who always know how to have fun. I also attend online classes for my AAS in Web Development, which I will be receiving in 2019.<br/><br/>
             Although I'm a front end developer I like challenges of all sorts so if you are looking for someone to work with, use the fancy contact form to get in touch!</p>
-            <a href="http://iamtimsmith.com/TimSmith_resume.pdf" target="_blank" className="button">My Resume</a>
+            <a href="resume.pdf" target="_blank" className="button">My Resume</a>
           </div>
           <div className="column">
             <Contact buttonText="Hire Me!" />
