eee8e184c3bfec373f08f205098bedd97d089bbd
Tim Smith <tgsmith489@gmail.com>
2019-01-08 08:59:00 -0600

    Updated Blog Post template for CMS

--- src/cms/templates/BlogPostPreview.js
+++ src/cms/templates/BlogPostPreview.js
@@ -1,13 +1,40 @@
 import React from "react";
-import BlogPostTemplate from "../../templates/blogPost";
+import SocialShare from "../../components/socialShare";
 
 const BlogPostPreview = ({ entry, widgetFor }) => (
-  <BlogPostTemplate
-    content={widgetFor("body")}
-    description={entry.getIn(["data", "description"])}
-    tags={entry.getIn(["data", "tags"])}
-    title={entry.getIn(["data", "title"])}
-  />
+  <div>
+    <section
+      className="hero is-large"
+      style={{
+        backgroundImage: `url("${entry.getIn(["data", "image"])}")`
+      }}
+    >
+      <div className="hero-body container" />
+    </section>
+    <section className="section">
+      <div className="container">
+        <h1 className="is-size-1">{entry.getIn(["data", "title"])}</h1>
+        <div className="columns">
+          <div className="column is-narrow">
+            <p className="is-size-5">{entry.getIn(["data", "date"])}</p>
+          </div>
+        </div>
+
+        <hr />
+        <br />
+        <div id="post-content">{widgetFor("body")}</div>
+        <br />
+      </div>
+      <SocialShare
+        url={`https://www.iamtimsmith.com/blog/${entry.getIn([
+          "data",
+          "path"
+        ])}`}
+        image={`https://www.iamtimsmith.com/${entry.getIn(["data", "image"])}`}
+        title={entry.getIn(["data", "title"])}
+      />
+    </section>
+  </div>
 );
 
 export default BlogPostPreview;
