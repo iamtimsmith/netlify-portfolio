3e8534d80674d73af75e1378bfdd5561c63c463e
Tim Smith <tgsmith489@gmail.com>
2019-01-08 10:05:02 -0600

    Another attempt to get styles added to preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,5 @@
 import React from "react";
 import CMS from "netlify-cms";
-import "netlify-cms/dist/cms.css";
 import "./templates/BlogPostStyles.css";
 import BlogPostPreview from "./templates/BlogPostPreview";
 
