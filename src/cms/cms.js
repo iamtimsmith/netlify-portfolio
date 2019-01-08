2a9c4292b97ad36d8bfbd45d1f0fa7b5c26d5cf8
Tim Smith <tgsmith489@gmail.com>
2019-01-08 09:33:32 -0600

    Another attempt to get styles added to preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,6 @@
 import CMS from "netlify-cms";
 
 import BlogPostPreview from "./templates/BlogPostPreview";
+import "../../src/templates/blogPost/index.scss";
 
 CMS.registerPreviewTemplate("blog", BlogPostPreview);
-CMS.registerPreviewStyle("./templates/BlogPostStyles.css");
