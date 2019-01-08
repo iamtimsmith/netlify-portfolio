6c81d2a83ba25b4af56d473dae9514ae5575686b
Tim Smith <tgsmith489@gmail.com>
2019-01-08 09:20:10 -0600

    Trying different solution for Blog Post Styles in CMS

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,7 +1,6 @@
 import CMS from "netlify-cms";
 
 import BlogPostPreview from "./templates/BlogPostPreview";
-import BlogPostStyles from "./templates/BlogPostStyles.css";
 
 CMS.registerPreviewTemplate("blog", BlogPostPreview);
-CMS.registerPreviewStyle(BlogPostStyles);
+CMS.registerPreviewStyle("./templates/BlogPostStyles.css");
